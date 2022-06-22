591e2cc2246aeb455f00654fd7b5c9cd9893b840
CuetheGreat <c.stewart89@gmail.com>
2022-06-22 15:54:32 -0400

    Changed scope in routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :categories
+  scope 'api/v1' do
+    resources :events
+    resources :categories
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
